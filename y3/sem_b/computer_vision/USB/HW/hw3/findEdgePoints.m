f648010eedfbc19cd122fb33cceceb477c6e7c05
Noam Stolero <stellern@gmail.com>
2018-07-18 12:20:01 +0300

    Clean up

--- y3/sem_b/computer_vision/USB/HW/hw3/findEdgePoints.m
+++
@@ -1,8 +0,0 @@
-function [ ePoints ] = findEdgePoints( im )
-
-    [I,J] = find(im>0);
-    ePoints = [I J] - size(im)/2;
-
-
-end
-
--- y3/sem_b/computer_vision/USB/HW/hw5/functions/laplace_perim.m
+++
@@ -1,8 +0,0 @@
-function [ perim ] = laplace_perim( image, val )
-
-image(image ~= val) = 255;
-dseg = double(image);
-del_seg = del2(dseg);
-perim = length(find(del_seg<0));
-end
-
